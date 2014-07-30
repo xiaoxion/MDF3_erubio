eb754bb6f8abf7bff554e4e001ae4b855ebec22b
xiaoxion <esaurubio@fullsail.edu>
2014-07-30 16:40:41 -0400

    Revert 78ed8e8..a44d00f
    This rolls back to commit 78ed8e8da8e8a2830f63e128374dbcb70a156567.

---
+++ Week4/app/src/main/java/com/stratazima/workganize/fragments/FileSaveFragment.java
@@ -0,0 +1,34 @@
+package com.stratazima.workganize.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.stratazima.workganize.R;
+
+/**
+ * Created by esaurubio on 7/29/14.
+ */
+public class FileSaveFragment extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public static FileSaveFragment newInstance(int sectionNumber) {
+        FileSaveFragment fragment = new FileSaveFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public FileSaveFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_data, container, false);
+        return rootView;
+    }
+}
